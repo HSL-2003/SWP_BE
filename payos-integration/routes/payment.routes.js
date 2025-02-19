db336f5b2b5665fcc1c7b978450a9be7793ac5c2
Tôn Thất Trung Hiếu <trunghieutonthat@gmail.com>
2025-02-20 02:59:50 +0700

    fix payos

--- payos-integration/routes/payment.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const paymentController = require('../controllers/payment.controller');
-
-router.post('/create', paymentController.createPayment);
-router.post('/webhook', paymentController.handleWebhook);
-
-module.exports = router; 
